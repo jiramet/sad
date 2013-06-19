4ed20c49fd82e3e045c752ac862e67cfcfd81dd7
Thirachai <mandmod@gmail.com>
2013-06-19 11:41:42 +0700

    start Project to New computer

--- default.php
+++ default.php
@@ -14,47 +14,47 @@ $sum_title = $db->fetch($db->query("SELECT COUNT(t_id) AS data FROM "._TITLE."")
 
 
 <?php
-$sum_product = $db->fetch($db->query("SELECT COUNT(p_id) AS data FROM "._PRODUCT.""));
+$sum_title = $db->fetch($db->query("SELECT COUNT(t_id) AS data FROM "._TITLE.""));
 ?>
-<div class="dot-footer">มีสินค้าอยู่ในระบบทั้งหมด  <span class="bold blue"><?php echo $sum_product->data; ?></span> ชนิด</div>
+<div class="dot-footer">มีสินค้าอยู่ในระบบทั้งหมด  <span class="bold blue"><?php echo $sum_title->data; ?></span> ชนิด</div>
 
 
 <?php
-$sum_order = $db->fetch($db->query("SELECT COUNT(o_id) AS data FROM "._ORDER.""));
+$sum_title = $db->fetch($db->query("SELECT COUNT(t_id) AS data FROM "._TITLE.""));
 ?>
-<div class="dot-footer">และมีบันทึกการขายทั้งสิ้น  <span class="bold blue"><?php echo $sum_order->data; ?></span> รายการ</div>
+<div class="dot-footer">และมีบันทึกการขายทั้งสิ้น  <span class="bold blue"><?php echo $sum_title->data; ?></span> รายการ</div>
 
 <BR><BR>
 <?php
-$sum_product = $db->fetch($db->query("SELECT SUM(p_all) AS sum FROM "._PRODUCT.""));
+$sum_title = $db->fetch($db->query("SELECT COUNT(t_id) AS data FROM "._TITLE.""));
 ?>
-<div class="dot-footer">จากข้อมูลพบว่า มีสินค้าทั้งหมดในระบบ <span class="bold blue"><?php echo $sum_product->sum?$sum_product->sum:0; ?></span> ชิ้น</div>
+<div class="dot-footer">จากข้อมูลพบว่า มีสินค้าทั้งหมดในระบบ <span class="bold blue"><?php echo $sum_title->data; ?></span> ชิ้น</div>
 
 <?php
-$sum_order = $db->fetch($db->query("SELECT SUM(o_item) AS sum FROM "._ORDER.""));
+$sum_title = $db->fetch($db->query("SELECT COUNT(t_id) AS data FROM "._TITLE.""));
 ?>
-<div class="dot-footer">และมียอดขายไปแล้ว <span class="bold blue"><?php echo $sum_order->sum?$sum_order->sum:0; ?></span> ชิ้น</div>
+<div class="dot-footer">และมียอดขายไปแล้ว <span class="bold blue"><?php echo $sum_title->data; ?></span> ชิ้น</div>
 
 <?php
-$sum_price = $db->fetch($db->query("SELECT SUM(o_price) AS sum FROM "._ORDER.""));
+$sum_title = $db->fetch($db->query("SELECT COUNT(t_id) AS data FROM "._TITLE.""));
 ?>
-<div class="dot-footer">รวมเป็นเงินมูลค่าทั้งสิ้น <span class="bold blue"><?php echo $sum_price->sum?$sum_price->sum:0; ?></span> บาท</div>
+<div class="dot-footer">รวมเป็นเงินมูลค่าทั้งสิ้น <span class="bold blue"><?php echo $sum_title->data; ?></span> บาท</div>
 
 
 <?php
-$sum_price = $db->fetch($db->query("SELECT SUM(o_price) AS sum FROM "._ORDER." WHERE o_status IS NOT NULL"));
+$sum_title = $db->fetch($db->query("SELECT COUNT(t_id) AS data FROM "._TITLE.""));
 ?>
-<div class="dot-footer">เป็นเงินที่ชำระแล้ว <span class="bold blue"><?php echo $sum_price->sum?$sum_price->sum:0; ?></span> บาท</div>
+<div class="dot-footer">เป็นเงินที่ชำระแล้ว <span class="bold blue"><?php echo $sum_title->data; ?></span> บาท</div>
 
 <?php
-$sum_price = $db->fetch($db->query("SELECT SUM(o_price) AS sum FROM "._ORDER." WHERE o_status IS NULL"));
+$sum_title = $db->fetch($db->query("SELECT COUNT(t_id) AS data FROM "._TITLE.""));
 ?>
-<div class="dot-footer">และยังไม่ได้ชำระอีก <span class="bold red"><?php echo $sum_price->sum?$sum_price->sum:0; ?></span> บาท</div>
+<div class="dot-footer">และยังไม่ได้ชำระอีก <span class="bold red"><?php echo $sum_title->data; ?></span> บาท</div>
 
 <?php
-$sum_price = $db->fetch($db->query("SELECT COUNT(o_price) AS sum FROM "._ORDER." WHERE o_status IS NULL"));
+$sum_title = $db->fetch($db->query("SELECT COUNT(t_id) AS data FROM "._TITLE.""));
 ?>
-<div class="dot-footer">โดยรวมรายการที่ยังไมได้ชำระทั้งสิ้น <span class="bold pink"><?php echo $sum_price->sum?$sum_price->sum:0; ?></span> รายการ</div>
+<div class="dot-footer">โดยรวมรายการที่ยังไมได้ชำระทั้งสิ้น <span class="bold pink"><?php echo $sum_title->data; ?></span> รายการ</div>
 
 
 
--- function.php
+++ function.php
@@ -22,52 +22,52 @@ function SplitPage($page = 1,$totalpage = 1,$option = ""){
 			echo 'ทั้งหมด '.$totalpage.' หน้า';
 			echo ' | <a href="'.$option.''.$totalpage.'" target="_self">หน้าสุดท้าย <span class="nexticon">&nbsp;</span></a> </div>';
 		}
-		
+
 		if($page == 1){
-		
+
 			echo '<span class="preicon">&nbsp;</span>| <span class="tal bold f16">'.$page.'</span> | ';
 			for($l = 2; $l <= $totalpage; $l++){
 				echo '<a href="'.$option.''.$l.'" target="_self">'.$l.'</a> | ';
 			}
-			
+
 			echo '<a href="'.$option.''.($page+1).'" target="_self"> <span class="nexticon">&nbsp;</span></a>';
-			
-			
+
+
 		}else if($page == $totalpage){
 			echo '<a href="'.$option.''.($page-1).'" target="_self"><span class="preicon"></span></a> | ';
-				
+
 			for($l = 1; $l < $totalpage; $l++){
 				echo '<a href="'.$option.''.$l.'" target="_self">'.$l.'</a> | ';
 			}
-			
+
 			echo '<span class="tal bold f16">'.$page.'</span> | <span class="nexticon">&nbsp;</span>';
-			
+
 		}else{
 			echo '<a href="'.$option.''.($page-1).'" target="_self"><span class="preicon"></span></a> | ';
-			
+
 			for($l = 1 ; $l < $page; $l++){
 				echo '<a href="'.$option.''.$l.'" target="_self">'.$l.'</a> | ';
 			}
-			
+
 			echo '<span class="tal bold f16">'.$page.'</span> |';
-			
+
 			for($r = $page+1 ; $r <= $totalpage; $r++){
 				echo '<a href="'.$option.''.$r.'" target="_self">'.$r.'</a> | ';
 			}
-			
+
 			echo '<a href="'.$option.''.($page+1).'" target="_self"> <span class="nexticon">&nbsp;</span></a>';
-			
+
 		}
 	}else{
-		
+
 		if($totalpage >= 3){
 			echo '<div><a href="'.$option.'1" target="_self"><span class="preicon">หน้าแรก</span></a> | ';
 			echo 'ทั้งหมด '.$totalpage.' หน้า';
 			echo ' | <a href="'.$option.''.$totalpage.'" target="_self">หน้าสุดท้าย <span class="nexticon">&nbsp;</span></a> </div>';
 		}
-		
+
 		if($page == 1){
-		
+
 			echo '<span class="preicon">&nbsp;</span>| <span class="tal bold f16">'.$page.'</span> | ';
 			for($l = 2; $l <= 5; $l++){
 				echo '<a href="'.$option.''.$l.'" target="_self">'.$l.'</a> | ';
@@ -76,13 +76,13 @@ function SplitPage($page = 1,$totalpage = 1,$option = ""){
 			for($r = $totalpage - 4; $r <= $totalpage; $r++){
 				echo '<a href="'.$option.''.$r.'" target="_self">'.$r.'</a> | ';
 			}
-			
+
 			echo '<a href="'.$option.''.($page+1).'" target="_self"> <span class="nexticon">&nbsp;</span></a>';
-			
-			
+
+
 		}else if($page == $totalpage){
 			echo '<a href="'.$option.''.($page-1).'" target="_self"><span class="preicon"></span></a> | ';
-				
+
 			for($l = 1; $l <= 5; $l++){
 				echo '<a href="'.$option.''.$l.'" target="_self">'.$l.'</a> | ';
 			}
@@ -90,26 +90,26 @@ function SplitPage($page = 1,$totalpage = 1,$option = ""){
 			for($r = $totalpage - 4; $r < $totalpage; $r++){
 				echo '<a href="'.$option.''.$r.'" target="_self">'.$r.'</a> | ';
 			}
-			
+
 			echo '<span class="tal bold f16">'.$page.'</span> | <span class="nexticon">&nbsp;</span>';
-			
+
 		}else{
-			
+
 			if($page > 1 and $page < 10){
 				echo '<a href="'.$option.''.($page-1).'" target="_self"><span class="preicon"></span></a> | ';
-			
+
 				for($l = 1 ; $l < $page; $l++){
 					echo '<a href="'.$option.''.$l.'" target="_self">'.$l.'</a> | ';
 				}
-				
+
 				echo '<span class="tal bold f16">'.$page.'</span> | ';
-				
+
 				for($r = $page+1 ; $r <= 10; $r++){
 					echo '<a href="'.$option.''.$r.'" target="_self">'.$r.'</a> | ';
 				}
 				echo '... | ';
 				echo '<a href="'.$option.''.$totalpage.'" target="_self">'.$totalpage.'</a> | ';
-				
+
 				echo '<a href="'.$option.''.($page+1).'" target="_self"> <span class="nexticon">&nbsp;</span></a>';
 			}else if($page >= $totalpage - 8 and $page < $totalpage){
 				echo '<a href="'.$option.''.($page-1).'" target="_self"><span class="preicon"></span></a> | ';
@@ -129,22 +129,22 @@ function SplitPage($page = 1,$totalpage = 1,$option = ""){
 					echo '<a href="'.$option.''.$l.'" target="_self">'.$l.'</a> | ';
 				}
 				echo '... | ';
-				echo '<a href="'.$option.''.($page-1).'" target="_self">'.($page-1).'</a> | '; 
+				echo '<a href="'.$option.''.($page-1).'" target="_self">'.($page-1).'</a> | ';
 				echo '<span class="tal bold f16">'.$page.'</span> | ';
 				echo '<a href="'.$option.''.($page+1).'" target="_self">'.($page+1).'</a> | ';
 				echo '... | ';
-				
+
 				for($r = $totalpage - 2; $r <= $totalpage; $r++){
 					echo '<a href="'.$option.''.$r.'" target="_self">'.$r.'</a> | ';
 				}
-				
+
 				echo '<a href="'.$option.''.($page+1).'" target="_self"> <span class="nexticon">&nbsp;</span></a>';
-			}			
+			}
 		}
 	}
 }
 		//======================จบFunction การแบ่งหน้า===============================
-		
+
 
 	#Mod เขียนนะครับ
 	# function  Check ว่า  มีค่า 1 หรือ เป็นค่าว่าง  ถ้าเป็นค่าว่างให้ มีค่า = 0
@@ -159,7 +159,7 @@ function SplitPage($page = 1,$totalpage = 1,$option = ""){
 			return $ret;
 		}
 	}
-	
+
 	#Permission
 	#action = add edit del open
 	#permiss = เป็นข้อมูลที่ดึงจาก Databases มีค่า 1 กับ 0
@@ -181,8 +181,6 @@ function SplitPage($page = 1,$totalpage = 1,$option = ""){
 	$objDB = mysql_select_db($db);
 
 	function fperurl($f, $action, $id, $text, $permiss, $page) {
-		
-	
 		$strSQL = "SELECT * FROM wurl WHERE w_url="."'$f'";
 		$objQuery = @mysql_query($strSQL);
 		$ret = @mysql_fetch_array($objQuery);
@@ -193,8 +191,8 @@ function SplitPage($page = 1,$totalpage = 1,$option = ""){
 		$objQuery1 = @mysql_query($strSQL1);
 		$ret1 = @mysql_fetch_array($objQuery1);
 		$ret2 = $ret1[mp_wper];
-		
-		
+
+
 		// select w1.w_id, w1.w_url, m1.mp_mid, m1.mp_wid from wurl w1, mpermission m1  where m1.mp_mid = "14" and m1.mp_wid = "1"
 
 		if (!(empty($action)) and !(empty($id)) and $permiss == 1) { 			// เปิดดู แก้ไข ลบ   มีค่า ใน Action   และ id
@@ -203,7 +201,7 @@ function SplitPage($page = 1,$totalpage = 1,$option = ""){
 				return $ret;
 			}elseif ($action == 'del'){      																// ลบอย่างเดียวเลย
 				$ret=  ' [ <a href="index.php?f=' . $f. '&action='.$action.'&id='.$id.'&page='.$page. ' " onclick="return confirm(\'คุณต้องการลบข้อมูลนี้ใช่หรือไม่ ?\')" >ลบ</a> ] |';
-				return $ret;				
+				return $ret;
 			}
 		} elseif (!(empty($action)) and empty($id) and $permiss == 1) {  			//เพิ่มข้อมูล มีค่าใน Act แต่ id ไม่มีค่า
 			$ret = ' [ <a href="index.php?f='.$f.'&action='.$action.'#link1">'.$text.'</a> ]';
@@ -217,11 +215,11 @@ function SplitPage($page = 1,$totalpage = 1,$option = ""){
 		}
 
 	}
-	
+
 			//======================Function เช็คสิทธิในการเข้าถึง หน้า Page นั้น ๆ===============================
 	/*
-	 * มดเขียนครับ  
-	 * function 
+	 * มดเขียนครับ
+	 * function
 	 */
 	function fpermiss($f){
 		//$ret = $db->fetch($db->query("SELECT * FROM "._WURL." WHERE w_url="."'$f'"));
@@ -230,7 +228,7 @@ function SplitPage($page = 1,$totalpage = 1,$option = ""){
 		$ret = @mysql_fetch_array($objQuery);
 		//$ret3 = $ret[w_id];
 		//return $ret3;
-		 
+
 //	$strSQL1 = "SELECT * FROM "._MPER."  WHERE mp_mid = ".$_SESSION["id"]." and mp_wid="."'$ret[w_id]'" ;
 	$strSQL1 = "SELECT * FROM "._MPER."  WHERE mp_mid = ".$_SESSION["gpid"]." and mp_wid="."'$ret[w_id]'" ;
 		$objQuery1 = @mysql_query($strSQL1);
@@ -241,14 +239,14 @@ function SplitPage($page = 1,$totalpage = 1,$option = ""){
 			getJavaAlert("คุณไม่มีสิทธิ เข้า หน้านี้ครับ  อะ อะ อย่า Hack กันนิ สาด....", 0);
 			_go("index.php");
 		}
-	
+
 	}
 				//จบFunction เช็คสิทธิในการเข้าถึง หน้า Page นั้น ๆ
-				
+
 				//=========Function ในการตรวจสอบ การเข้าถึง ภายใน Page เช่น เพิ่ม, แก้ไข, ลบ, และ การจัดการสิทธิ===========================
 				//finwebper ย่อมาจาก  Function in web permission
 				function finwebper($f,$action) {
-					
+
 					$strSQL = "SELECT * FROM "._WURL." WHERE w_url="."'$f'";
 					$objQuery = @mysql_query($strSQL);
 					$ret = @mysql_fetch_array($objQuery);
@@ -274,45 +272,45 @@ function SplitPage($page = 1,$totalpage = 1,$option = ""){
 						getJavaAlert("คุณไม่มีสิทธิ เข้า หน้านี้ครับ  อะ อะ อย่า Hack กันนิ สาด....5", 0);
 						_go("index.php");
 					}
-					
-					
-				}		
-	
-	
-	
+
+
+				}
+
+
+
 		      	//จบ===Function ในการตรวจสอบ การเข้าถึง ภายใน Page เช่น เพิ่ม, แก้ไข, ลบ, และ การจัดการสิทธิ
 
-				
-				
-				
-				
-				//======================Function ดึงรายชื่อ Field ใน Mysql ออกมาใช้งาสน===============================
-				
+
+
+
+
+				//======================Function ดึงรายชื่อ Field ใน Mysql ออกมาใช้งาน===============================
+
 	//Function หาจำนวน Field ใน Mysql
 	function getsumfieldmysql($value1) {
 		$strSQL = "SELECT * FROM ".$value1;
 		$objQuery = mysql_query($strSQL) or die("Error Query [".$strSQL."]");
 		$intNumField = mysql_num_fields($objQuery);
-		return $intNumField;		
+		return $intNumField;
 		mysql_close($objConnect);
 	}
-	
+
 	function getfieldmysql($value1) {
 		$strSQL = "SELECT * FROM ".$value1;
 		$objQuery = mysql_query($strSQL) or die("Error Query [".$strSQL."]");
 		$intNumField = mysql_num_fields($objQuery);
-		$i = 0;		
+		$i = 0;
 		$valu = array();
-		for ($i = 0; $i <= $intNumField; $i++) {
+		for ($i = 0; $i < $intNumField; $i++) {
 			$j=$i+1;
 			$valu[$j] = mysql_field_name($objQuery, $i);
 		}
 		return $valu;
 		mysql_close($objConnect);
 	}
-	
-				
-	
-	
+
+
+
+
 
 ?>
