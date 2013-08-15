70aefa7d77b8aa891ad5ea51bcfe8c9cb39e4a0c
Thirachai <mandmod@gmail.com>
2013-08-15 10:58:35 +0700

    test

--- phpqrcode/test.php
+++ phpqrcode/test.php
@@ -1,7 +1,28 @@
 <?php
-//include ('../lib/full/qrlib.php');
+// include ('../lib/full/qrlib.php');
+// header("Content-type: image/png");
 include ('qrlib.php');
 
-// outputs image directly into browser, as PNG stream
-QRcode::png ( 'PHP QR Code :)' );
+include ('config.php');
+
+// how to save PNG codes to server
+
+$tempDir = './image/png';
+
+$codeContents = 'www.google.co.th';
+
+// we need to generate filename somehow,
+// with md5 or with database ID used to obtains $codeContents...
+$fileName = '005_file_' . md5 ( $codeContents ) . '.png';
+
+$pngAbsoluteFilePath = $tempDir . $fileName;
+$urlRelativeFilePath = './image/png' . $fileName;
+
+	QRcode::png ( $codeContents, $pngAbsoluteFilePath );
+
+echo 'Server PNG File: ' . $pngAbsoluteFilePath;
+echo '<hr />';
+
+// displaying
+echo '<img src="' . $urlRelativeFilePath . '" />';
 ?>
